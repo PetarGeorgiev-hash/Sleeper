4bf9fff86ed5e0b0b8a4fd731293b0e67d437184
Petar Georgiev <petiyo07@abv.bg>
2025-07-15 15:00:31 +0300

    dump all files

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UserDocument, UserSchema } from './models/user.schema';
+import { DatabaseModule } from '@app/common';
+import { UserRepository } from './users.repository';
+
+@Module({
+   imports: [
+     DatabaseModule,
+     DatabaseModule.forFeature([{name: UserDocument.name, schema: UserSchema}])],
+  controllers: [UsersController],
+  providers: [UsersService, UserRepository],
+  exports: [UsersService]
+})
+export class UsersModule {}
---
+++ apps/reservations/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { ReservationsModule } from './reservations.module';
+import {  ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
+async function bootstrap() {
+  const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.useLogger(app.get(Logger))
+  const config = app.get(ConfigService)
+  await app.listen(config.get("PORT"));
+}
+bootstrap();
