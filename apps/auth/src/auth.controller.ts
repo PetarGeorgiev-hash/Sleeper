66e805626fb5dcf2debd9d0805395d912088b759
Petar Georgiev <petiyo07@abv.bg>
2025-07-24 16:12:02 +0300

    fix yaml file

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -22,7 +22,6 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @MessagePattern('authenticate')
   async authenticate(@Payload() data: any) {
-    //test
     console.log(data)
     return data.user
   }
